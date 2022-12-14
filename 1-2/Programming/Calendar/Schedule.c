867ead2592e526305ed6254e43969678b20c1f10
JunSeob Kim <46683933+BenzeneKim@users.noreply.github.com>
2022-12-14 15:59:43 +0900

    Update Schedule.c
    add get schedule function

--- 1-2/Programming/Calendar/Schedule.c
+++ 1-2/Programming/Calendar/Schedule.c
@@ -6,19 +6,74 @@
 #include "Schedule.h"
 #include "main.h"
 
-char* GetSchedule(Day pointingDay) {
+
+/*====database format
+#n
+yyyymmddhhmm   (date)(time)
+schedule txt
+
+#n+1
+.
+.
+.
+*/
+void GetSchedule(Schedule* scheduleArr) {
 	FILE* fpR;
-    int isPressed;
-	fopen_s(&fpR, "./Database.txt", "rt");
+	int	scheduleNum;
+	char txtBuffer[10000];
+	fopen_s(&fpR, "./Database.txt", "r");
+	//get the number of schedules
 	if (fpR != NULL) {
+		//get the number of schedules
+		while(!feof(rpR)){
+			fgets(txtBuffer, 10000,fpR);
+			scheduleNum++;
+		}
+		scheduleNum/=4;
+		fclose(fpR);
+		scheduleArr=(*Schedule)calloc(scheduleNum, sizeof(*Schedule));
+		fopen(&fpR, "/Database.txt", "r");
+		int	scheduleInd=0;
+		while(!feof(rpR)){
+			fgets(txtBuffer,10000,fpR);
+			if(txtBuffer[0]=='#'){
+				char year[5];
+				char month[3];
+				char date[3];
+				char hour[3];
+				char minute[3];
+				fgets(txtBuffer,10000,fpR);
+				strncpy(year,txtBuffer,4);
+				eraseStrFL(txtBuffer,4);
+				strncpy(month,txtBuffer,2);
+				eraseStrFL(txtBuffer,2);
+				strncpy(day,txtBuffer,2);
+				eraseStrFL(txtBuffer,2);
+				strncpy(hour,txtBuffer,2);
+				eraseStrFL(txtBuffer,2);
+				strncpy(minute,txtBuffer,2);
+				scheduleArr[scheduleInd].pointingDay.year=itoa(year);
+				scheduleArr[scheduleInd].pointingDay.month=itoa(month);
+				scheduleArr[scheduleInd].pointingDay.date=itoa(date);
+				scheduleArr[scheduleInd].hour=itoa(hour);
+				scheduleArr[scheduleInd].minute=itoa(minute);
+				fgets(txtBuffer,10000,fpR);
+				scheduleArr[scheduleArr].contents=(char*)calloc(strlen(txtBuffer),sizeof(char*));
+				strcpy(scheduleArr[scheduleArr].contents,txtBuffer);
+			}
+		}
+	}
+	else *scheduleArr=0;
+}
 
-        while (isPressed == 0) {
-            if (kbhit()) {
-
-            }
-        }
-        return getKey;
-    }
+			   
+void eraseStrFL(char *targetS,int times){
+	for(int i=0;i<times;i++){
+		int	length=strlen(targetS)
+		for(int j=0;j<length-1;j++){
+			targetS[j]=target[j+1);
+		}
+		targetS[length-1]=NULL;
 	}
 }
 
